632b540031dff7957e4af518c826c88be24a5e98
Cesar Miranda <cesarmirandacx602@gmail.com>
2023-10-24 14:52:38 -0700

    added addNumbers function

--- functions/more_functions.js
+++ functions/more_functions.js
@@ -8,3 +8,11 @@ function displayMailingLabel(firstName, address, city, state, zip) {
 }
 
 let malingLabel = displayMailingLabel("Cesar ", "300 E Taylor St ","Phoenix ", "Arizona", "85002")
+
+// function addNumbers 
+function addNumbers(num1, num2) {
+    let numbersAddition= num1 + num2;
+    console.log(num1 + "+" + num2 + "=" + numbersAddition);
+}
+
+let numbersAdded = addNumbers(10, 21);
